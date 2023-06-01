918758e09206dd81de0c698d2b81f1f838a58a33
ryan <ryan.mason814@gmail.com>
2023-05-31 20:06:45 -0500

    detour through astronaut land

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use dotenv::dotenv;
-use std::env; 
-use reqwest;
+use std::env;
+use reqwest::blocking::get;
 
 
- #[tokio::main]
- async fn main() ->  Result<(), Box<dyn std::error::Error>>{ 
-// fn main() ->  Result<(), Box<dyn std::error::Error>>{ 
+//  #[tokio::main]
+//  async fn main() ->  Result<(), Box<dyn std::error::Error>>{ 
+fn main() -> Result<(), Box<dyn std::error::Error>> { 
+    _ = _sample_blocked_api_call_astronauts().is_ok();  
+
+    Ok(())
+}
+
+
+
+fn get_weather() -> Result<(), Box<dyn std::error::Error>>{
     const LAT: f64 = 33.236228;
     const LON: f64 = -96.80111;
 
@@ -26,9 +34,26 @@ use reqwest;
 
     println!("{}\n", &openweather);
 
-    let body = reqwest::get(openweather).await?.bytes().await?;
+    let body: serde_json::Value = reqwest::blocking::get(openweather)?.json()?;
     println!("body = {:?}", body);
+    Ok(())
+}
 
+fn _sample_blocked_api_call_astronauts() -> Result<(), reqwest::Error>{
+    
+    let url = "http://api.open-notify.org/astros.json";
+    let resp: serde_json::Value = get(url)?.json()?;
+
+    let n = &resp["number"];
+    let n = n.as_u64().unwrap_or_default() as usize;
+    println!("number of astronauts: {n}");
+ 
+
+    for x in 0..n {
+        let people = &resp["people"][x]["name"];
+        let people = people.to_string().replace('"', "");
+        println!("{}. {}",x+1, people)
+    }; 
     Ok(())
 }
 
@@ -49,3 +74,4 @@ fn get_env_test() {
     };
     assert_eq!(test_key, "TEST123");
 }
+ 
