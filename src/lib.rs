134a184f7dd7a031e5a84939ec076de40151d9fc
1140982 <ryan.e.mason@raytheon.com>
2023-06-05 16:01:33 -0500

    reformatted project for clarity.

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+
+use dotenv::dotenv;
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn output_to_json(resp: &serde_json::Value, fname: String) -> Result<(), anyhow::Error> {
+    let mut file = File::create(fname).unwrap();
+    file.write_all(resp.to_string().as_bytes())?;
+    Ok(())
+}
+
+pub fn get_env(key: String) -> String {
+    dotenv().ok();
+    match env::var_os(&key) {
+        Some(v) => v.into_string().unwrap(),
+        None => panic!("{} is not set", key),
+    }
+}
+
+#[test]
+fn get_env_test() {
+    let test_key = get_env("TEST".to_string());
+    assert_eq!(test_key, "TEST123");
+}
